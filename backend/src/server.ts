32f523e3dcfdaaed38415f276d91c2f6b08ce257
joinwithyogeshRK <joinwithyogesh17@gmail.com>
2026-04-14 03:05:25 +0530

    cors fixed for mic

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+
 import { Router } from "express";
 import multer from "multer";
 import test from "./routes/pdf.js";
